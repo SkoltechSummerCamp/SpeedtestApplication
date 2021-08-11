9b7c3fe2ceac0d3b4075629018b00177fdb90b82
Kazakov Mihail <advancerman@gmail.com>
2021-08-11 23:40:24 +0700

    Add iperf sources as submodule

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,42 +6,43 @@ LOCAL_CFLAGS := -DHAVE_CONFIG_H -UAF_INET6 -w -Wno-error=format-security
 LOCAL_LDFLAGS := -fPIE -pie
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_SRC_FILES := \
-        ../cpp/compat/delay.c \
-        ../cpp/compat/error.c \
-        ../cpp/compat/gettimeofday.c \
-        ../cpp/compat/inet_ntop.c \
-        ../cpp/compat/inet_pton.c \
-        ../cpp/compat/signal.c \
-        ../cpp/compat/snprintf.c \
-        ../cpp/compat/string.c \
-        ../cpp/compat/Thread.c \
-        ../cpp/src/Extractor.c \
-        ../cpp/src/gnu_getopt_long.c \
-        ../cpp/src/gnu_getopt.c \
-        ../cpp/src/histogram.c \
-        ../cpp/src/Locale.c \
-        ../cpp/src/pdfs.c \
-        ../cpp/src/ReportCSV.c \
-        ../cpp/src/ReportDefault.c \
-        ../cpp/src/Reporter.c \
-        ../cpp/src/service.c \
-        ../cpp/src/SocketAddr.c \
-        ../cpp/src/sockets.c \
-        ../cpp/src/stdio.c \
-        ../cpp/src/tcp_window_size.c \
-        ../cpp/src/Client.cpp \
-        ../cpp/src/isochronous.cpp \
-        ../cpp/src/Launch.cpp \
-        ../cpp/src/List.cpp \
-        ../cpp/src/PerfSocket.cpp \
-        ../cpp/src/Settings.cpp \
-        ../cpp/src/main.cpp \
-        ../cpp/src/Listener.cpp \
-        ../cpp/src/Server.cpp
+        ../../../../iPerf/compat/delay.c \
+        ../../../../iPerf/compat/error.c \
+        ../../../../iPerf/compat/gettimeofday.c \
+        ../../../../iPerf/compat/inet_ntop.c \
+        ../../../../iPerf/compat/inet_pton.c \
+        ../../../../iPerf/compat/signal.c \
+        ../../../../iPerf/compat/snprintf.c \
+        ../../../../iPerf/compat/string.c \
+        ../../../../iPerf/compat/Thread.c \
+        ../../../../iPerf/src/Extractor.c \
+        ../../../../iPerf/src/gnu_getopt_long.c \
+        ../../../../iPerf/src/gnu_getopt.c \
+        ../../../../iPerf/src/histogram.c \
+        ../../../../iPerf/src/Locale.c \
+        ../../../../iPerf/src/pdfs.c \
+        ../../../../iPerf/src/ReportCSV.c \
+        ../../../../iPerf/src/ReportDefault.c \
+        ../../../../iPerf/src/Reporter.c \
+        ../../../../iPerf/src/service.c \
+        ../../../../iPerf/src/SocketAddr.c \
+        ../../../../iPerf/src/sockets.c \
+        ../../../../iPerf/src/stdio.c \
+        ../../../../iPerf/src/tcp_window_size.c \
+        ../../../../iPerf/src/Client.cpp \
+        ../../../../iPerf/src/isochronous.cpp \
+        ../../../../iPerf/src/Launch.cpp \
+        ../../../../iPerf/src/List.cpp \
+        ../../../../iPerf/src/PerfSocket.cpp \
+        ../../../../iPerf/src/Settings.cpp \
+        ../../../../iPerf/src/main.cpp \
+        ../../../../iPerf/src/Listener.cpp \
+        ../../../../iPerf/src/Server.cpp \
+        ../cpp/src/mainJni.cpp \
 
 LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/../cpp \
-        $(LOCAL_PATH)/../cpp/include
+        $(LOCAL_PATH)/../../../../iPerf/include
 
 LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
 include $(BUILD_SHARED_LIBRARY)
